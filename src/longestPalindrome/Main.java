42f26d3fbdc0a00d3df8f889ad4eb27b992a9b69
cuikangyuan3 <cuikangyuan@jd.com>
2021-11-21 23:34:16 +0800

    add longestpalindrome

---
+++ src/longestPalindrome/Main.java
@@ -0,0 +1,12 @@
+package longestPalindrome;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        String s = "acaba";
+        String s1 = new LongestPalindrome().longestPalindrome(s);
+        System.out.println(s1);
+
+    }
+}
