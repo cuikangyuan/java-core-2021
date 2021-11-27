a9dc36dad80d71baea3af76db669390d476eace0
cuikangyuan3 <cuikangyuan@jd.com>
2021-11-27 22:15:52 +0800

    add UnionFind solution for matrix replace problem

--- src/UnionFind/Main.java
+++ src/UnionFind/Main.java
@@ -1,7 +1,29 @@
 package UnionFind;
 
+
 public class Main {
     public static void main(String[] args) {
+        char[][] board = {
+                new char[]{'X', 'X', 'X', 'X', '0'},
+                new char[]{'X', 'X', 'X', '0', 'X'},
+                new char[]{'0', '0', 'X', '0', 'X'},
+                new char[]{'X', '0', 'X', 'X', 'X'},
+        };
 
+        /*
+        * X X X X 0
+        * X X X X X
+        * 0 0 X X X
+        * X 0 X X X
+        * */
+
+        new UFMatrixReplace().solve(board);
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println("");
+        }
     }
 }
