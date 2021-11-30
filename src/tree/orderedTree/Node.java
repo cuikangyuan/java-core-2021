9609b6f0975a5587ba5c2b73cf0daca073bbd6bd
cuikangyuan3 <cuikangyuan@jd.com>
2021-11-30 23:51:05 +0800

    add tree code

---
+++ src/tree/orderedTree/Node.java
@@ -0,0 +1,27 @@
+package tree.orderedTree;
+
+public class Node {
+
+    public int data;
+    public Node left;
+    public Node right;
+
+    public void add(Node node) {
+        if (node == null) {
+            return;
+        }
+        if (node.data < this.data) {
+            if (this.left == null) {
+                this.left = node;
+            } else {
+                this.left.add(node);
+            }
+        } else {
+            if (this.right == null) {
+                this.right = node;
+            } else {
+                this.right.add(node);
+            }
+        }
+    }
+}
