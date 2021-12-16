06ca3f50a745beb718833a5785018eedd7a85f4f
cuikangyuan <cuikangyuan@outlook.com>
2021-12-16 14:00:02 +0800

    add test code

---
+++ src/bigNumberCalculate/Main.java
@@ -0,0 +1,19 @@
+package bigNumberCalculate;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] a = {9, 9};
+        int[] b = {1, 1};
+
+    }
+
+    private int[] add(int[] a, int[] c) {
+
+        int[] result = null;
+
+        return result;
+    }
+}
