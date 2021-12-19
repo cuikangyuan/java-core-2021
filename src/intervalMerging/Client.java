06fe96458e60cafef0893d15cdcea74c42ff1b01
cuikangyuan <cuikangyuan@outlook.com>
2021-12-19 18:34:14 +0800

    add intervals merging

---
+++ src/intervalMerging/Client.java
@@ -0,0 +1,23 @@
+package intervalMerging;
+
+import jdk.internal.util.xml.impl.Pair;
+
+public class Client {
+    public static void main(String[] args) {
+
+        Client client = new Client();
+        client.merge_intervals();
+    }
+
+    private void merge_intervals() {
+
+        int[][] intervals = {
+            {},
+            {},
+            {},
+            {},
+            
+        };
+    }
+    
+}
