37da2ad9032bbb8eb9472726d35e364c5a47fdcc
cuikangyuan <cuikangyuan@outlook.com>
2021-12-22 23:49:52 +0800

    add b_search code

---
+++ src/algorithm/bSearch/Client.java
@@ -0,0 +1,29 @@
+package algorithm.bSearch;
+
+public class Client {
+    public static void main(String[] args) {
+
+        Client client = new Client();
+
+        System.out.println(client.mySqrt(8));
+
+    }
+
+    public int mySqrt(int x) {
+
+        if(x == 1) {
+            return 1;
+        }
+        int l = 0, r = x/2;
+        int mid = 0;
+        while(l < r) {
+            mid = l + (r - l + 1) / 2;
+            if((long)mid * mid <= x) {
+                l = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+}
