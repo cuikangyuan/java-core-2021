e101a67a189a3fbd7e7fec6d3d2c5bd019b57a49
cuikangyuan <cuikangyuan@outlook.com>
2021-12-25 19:09:26 +0800

    add kmp

--- src/algorithm/stringMatch/Client.java
+++ src/algorithm/stringMatch/Client.java
@@ -1,4 +1,21 @@
 package algorithm.stringMatch;
 
 public class Client {
+
+    public static void main(String[] args) {
+        char[] p = {'a', 'b'};
+        char[] s = {'a', 'b', 'a', 'b', 'a', 'b'};
+
+        BF bm = new BF();
+        KMP kmp = new KMP();
+
+//        bm.find(s, p);
+        int m = 6;
+        int n = 2;
+        p = new char[]{' ', 'a', 'b'};
+        s = new char[]{' ', 'a', 'b', 'a', 'b', 'a', 'b'};
+
+        kmp.find(s, m , p, n);
+    }
+
 }
