a63161a1e39b618a033cfc67eb3d3c62c457c7bf
cuikangyuan <cuikangyuan@outlook.com>
2022-01-24 16:37:11 +0800

    add n queue solution 1

--- src/algorithm/dfs/AC843/Client.java
+++ src/algorithm/dfs/AC843/Client.java
@@ -1,8 +1,63 @@
 package algorithm.dfs.AC843;
 
+import java.security.Principal;
+
 //843 n皇后问题
 public class Client {
+
+
+
+
+
+    private static final int N = 20;
+    private int n;
+    private char[][] g;
+    private boolean[] col, dg, edg;
+
+    public Client() {
+        g = new char[N][N];
+        col = new boolean[N];
+        dg = new boolean[N];
+        edg = new boolean[N];
+    }
+
+    private void dfs(int u) {
+
+        if(u == n) {
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    System.out.print(g[i][j] + "\t");
+                }
+                System.out.println("");
+            }
+            System.out.println("----------------------------------------------------------");
+        } else {
+            for(int j = 0; j < n; j++) {
+                if(!col[j] && !dg[j + u] && !edg[n - j + u]) {
+                    g[u][j] = 'Q';
+                    col[j] = dg[j + u] = edg[n - j + u] = true;
+                    dfs(u+1);
+                    col[j] = dg[j + u] = edg[n - j + u] = false;
+                    g[u][j] = '.';
+
+                }
+            }
+        }
+
+    }
+
+    private void testNQueue() {
+        n = 8;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                g[i][j] = '.';
+            }
+        }
+        dfs(0);
+    }
+
     public static void main(String[] args) {
         //TODO
+        new Client().testNQueue();
     }
 }
