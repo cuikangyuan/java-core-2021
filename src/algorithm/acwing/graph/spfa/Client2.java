f5d8fabb0c845f20afe3355c3c0c8104192b4778
cuikangyuan <cuikangyuan@outlook.com>
2022-02-10 23:32:37 +0800

    add spfa

--- src/algorithm/acwing/graph/spfa/Client2.java
+++ src/algorithm/acwing/graph/spfa/Client2.java
@@ -4,8 +4,8 @@ import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Queue;
 
-//851. spfa求最短路
-public class Client {
+//852. spfa判断负环
+public class Client2 {
     
     private static final int INF = 0x3f3f3f3f;
     private static final int N = 100010;
@@ -17,6 +17,7 @@ public class Client {
 
     private boolean[] st;
     private int[] dist;
+    private int[] cnt;
 
     private void add_edge(int a, int b, int weight) {
         e[idx] = b;
@@ -25,7 +26,7 @@ public class Client {
         h[a] = idx ++;
     }
 
-    public Client() {
+    public Client2() {
         n = m = 3;
 
         h = new int[N];
@@ -34,33 +35,36 @@ public class Client {
         w = new int[N];
         st = new boolean[N];
         dist = new int[N];
-        
+        cnt = new int[N];
 
-        Arrays.fill(dist, INF);
+        //Arrays.fill(dist, INF);
         Arrays.fill(h, -1);
         
-        add_edge(1, 2, 5);
-        add_edge(2, 3, -3);
-        add_edge(1, 3, 4);
+        add_edge(1, 2, -1);
+        add_edge(2, 3, 4);
+        add_edge(3, 1, -5);
     }
 
     private void test() {
-        spfa();
 
-        if(dist[n] == INF) {
-            System.out.println("impossible");
+        if(spfa()) {
+            System.out.println("Yes");
         } else {
-            System.out.println(dist[n]);
+            System.out.println("No");
         }
     }
 
-    private void spfa() {
+    private boolean spfa() {
 
         Queue<Integer> queue = new ArrayDeque();
-        dist[1] = 0;
-        st[1] = true;
-        queue.add(1);
 
+
+        for(int i = 1; i <= n; i++) {
+            st[i] = true;
+            queue.add(i);
+        }
+
+        //dist[1] = 0;
         while(queue.size() > 0) {
             
             int t = queue.poll();
@@ -70,6 +74,10 @@ public class Client {
                 int b = e[i];
                 if(dist[b] > dist[t] + w[i]) {
                     dist[b] = dist[t] + w[i];
+                    cnt[b] = cnt[t] + 1;
+                    if(cnt[b] >= n) {
+                        return true;
+                    }
                     if(!st[b]) {
                         st[b] = true;
                         queue.add(b);
@@ -78,11 +86,12 @@ public class Client {
             }
         }
 
+        return false;
     }
 
     public static void main(String[] args) {
     
-        Client client = new Client();
+        Client2 client = new Client2();
         client.test();
     }
 }
