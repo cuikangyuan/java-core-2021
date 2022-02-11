ebd6dc2143cba6c05e43e1b73176f781ad52dde8
cuikangyuan <cuikangyuan@outlook.com>
2022-02-11 22:46:08 +0800

    add floyd

--- src/algorithm/acwing/graph/floyd/Client.java
+++ src/algorithm/acwing/graph/floyd/Client.java
@@ -1,5 +1,36 @@
 package algorithm.acwing.graph.floyd;
 
+
+//854.Floyd求最短路
 public class Client {
     
+    private static final int N = 210;
+    private static final int M = 20010;
+    private static final int INF = 0x3f3f3f3f;
+
+    private int n, m , q;
+    private int g[][];
+
+    public Client() {
+        g = new int[M][M];
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+               
+            }
+        }
+    }
+
+    private void test() {
+        floyd();
+    }
+
+
+    private void floyd() {
+
+    }
+
+    public static void main(String[] args) {
+        
+        Client client = new Client();
+    }
 }
