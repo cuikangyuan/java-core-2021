770dc7c3390c2708a3c0a201e79e8fb750a42971
cuikangyuan <cuikangyuan@outlook.com>
2022-02-12 23:40:52 +0800

    add prim and kruskal

---
+++ src/algorithm/acwing/graph/kruskal/Client.java
@@ -0,0 +1,9 @@
+package algorithm.acwing.graph.kruskal;
+
+//TODO 859.Kruskal算法求最小生成树
+
+public class Client {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/algorithm/acwing/graph/prim/Client.java
@@ -0,0 +1,8 @@
+package algorithm.acwing.graph.prim;
+
+//TODO 858.Prim算法求最小生成树
+public class Client {
+    public static void main(String[] args) {
+        
+    }
+}
