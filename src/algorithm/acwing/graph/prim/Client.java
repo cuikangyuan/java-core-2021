34599da5a684011b40694fb818679316ffbac6d8
cuikangyuan <cuikangyuan@outlook.com>
2022-02-13 12:53:55 +0800

    add comment

--- src/algorithm/acwing/graph/prim/Client.java
+++ src/algorithm/acwing/graph/prim/Client.java
@@ -1,6 +1,8 @@
 package algorithm.acwing.graph.prim;
 
 //TODO 858.Prim算法求最小生成树
+//如果是稠密图 直接使用朴素版Prim算法
+//如果是稀疏图 使用Kruskal算法
 public class Client {
     public static void main(String[] args) {
         
